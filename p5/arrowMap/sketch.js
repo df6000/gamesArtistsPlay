c6c492f2871cd3c3aab85091440dc026e0734b6b
Douglas Fleischut <df@Douglass-iMac.local>
2017-01-26 15:56:14 -0800

    images and more

---
+++ p5/arrowMap/sketch.js
@@ -0,0 +1,17 @@
+var array = [1,4,6,3];
+
+
+// array=array.map(function(x){return x+1;});
+
+array=array.map(x => x+1);
+
+console.log(array);
+
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
